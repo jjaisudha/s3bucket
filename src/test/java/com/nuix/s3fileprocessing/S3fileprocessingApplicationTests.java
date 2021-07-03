a3407815b400eeaa5de87194ecb3c0dde4db1351
Jaisudha Jeyaraman <36182728+jjaisudha@users.noreply.github.com>
2021-07-03 15:12:11 +1000

    Updating tests

--- src/test/java/com/nuix/s3fileprocessing/S3fileprocessingApplicationTests.java
+++ src/test/java/com/nuix/s3fileprocessing/S3fileprocessingApplicationTests.java
@@ -2,12 +2,16 @@ package com.nuix.s3fileprocessing;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.nuix.s3fileprocessing.service.S3ServiceImpl;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootTest
 @Ignore
@@ -17,16 +21,33 @@ class S3fileprocessingApplicationTests {
 	private S3ServiceImpl s3Service;
 	@Value("${s3.access.key.name}")
 	private String key;
+	File destDir = new File("src/main/resources/");
+	File filteredFilesDir = new File("src/main/resources/filtered");
 
-
+/*
 	@Test
 	void contextLoads() {
+	}*//*
+	@Test
+	void contextLoads() {
+	}*/
+
+	@Test
+	public void downloadTest() {
+		s3Service.downloadFile(key);
+		File[] files = destDir.listFiles(obj -> obj.isFile() && obj.getName().endsWith(".csv"));
+		Assert.assertTrue(files.length>0);
 	}
 
 	@Test
-	public void dowanloadTest() {
-		s3Service.downloadFile(key);
-
+	public void testSearchCsv(){
+		try {
+			s3Service.filterCsvFile("ellipsis");
+			File [] filteredFiles = filteredFilesDir.listFiles(obj -> obj.isFile() && obj.getName().endsWith(".csv"));
+			Assert.assertTrue(filteredFiles.length>0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
